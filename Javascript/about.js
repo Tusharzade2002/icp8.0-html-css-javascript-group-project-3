7befbc3dfe5cc5642deea4159a6192498944ad2e
Maheshwarigadakh <maheshwarigadakh2003@gmail.com>
2024-07-18 15:27:09 +0530

    add increse decrease function

--- Javascript/about.js
+++ Javascript/about.js
@@ -3,3 +3,17 @@ function modes(){
     element.classList.toggle("dark-mode");
 }
 
+function changes(action){
+            const qualityElement = document.getElementById("qualitytext");
+            let quality = parseInt(qualityElement.innerText)
+            if(quality==1 && action=='dec'){
+                alert('Quantity cannot be less than 1')
+                return
+            }
+            else if(quality==5 && action=='inc'){
+                    alert('Quantity cannot be greater than 5')
+                    return
+            }
+            qualityElement.innerText=action == 'inc' ? ++quality : --quality
+             
+        }
