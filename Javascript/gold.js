0017daf4f54ac2ad8796e3322a525878f5a55f1d
Nehachatap <nehakchatap@gmail.com>
2024-07-21 01:06:49 +0530

    js file created

--- Javascript/gold.js
+++ Javascript/gold.js
@@ -1,3 +1,17 @@
+function likeItem(button){
+  const span = button.querySelector('span');
+  let count = parseInt(span.textContent);
+
+  if (button.classList.contains('liked')){
+      count--;
+      button.classList.remove('liked');
+  } else {
+      count++;
+      button.classList.add('liked');
+  }
+
+  span.textContent = count;
+}
 let Image=document.getElementById("image-slider");
 let Images=['./../Images/image/Slider 1.webp','./../Images/image/slider2.webp','./../Images/image/slider3.webp','./../Images/image/Slider4.webp'];
     
@@ -8,28 +22,3 @@ let Images=['./../Images/image/Slider 1.webp','./../Images/image/slider2.webp','
         // console.log(random);
         Images.src=Images[random];
     }
-
-const quantityElements = document.querySelectorAll('.quantity');
-quantityElements.forEach((element) => {
-    let quantity = parseInt(element.innerText);
-    element.nextElementSibling.addEventListener('click', () => {
-        ChangeQuantity(element, 'inc');
-      });
-      element.previousElementSibling.addEventListener('click', () => {
-        ChangeQuantity(element, 'dec');
-      });
-    });
-
-  function ChangeQuantity(quantityElement, op) {
-    let quantity = parseInt(quantityElement.innerText);
-
-            if (quantity == 1 && op == "dec"){
-            alert('quantity cannot be less than 1')
-                return
-            }
-            else if (quantity == 5 && op == "inc"){
-            alert('quantity cannot be more than 5')
-                return
-            }
-                quantityElement.innerText = op ==='inc' ? ++ quantity : --quantity;
-        }
