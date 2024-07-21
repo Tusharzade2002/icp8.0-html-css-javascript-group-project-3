7d366bc122b5c235ca5ff024c8be8adef98d69e8
gaytrisanap <sanapgaytri@gmail.com>
2024-07-21 16:59:16 +0900

    Js apply to review page

---
+++ Javascript/review.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".review-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
