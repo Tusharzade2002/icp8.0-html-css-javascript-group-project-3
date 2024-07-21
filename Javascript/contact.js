70c46da04ffc21e7745da1a14dc17037fe2b5213
aditishelke77 <aditishelke2003@gmail.com>
2024-07-21 17:47:03 +0530

    js is apply

---
+++ Javascript/contact.js
@@ -0,0 +1,15 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+        alert('Please fill out all fields.');
+        return;
+    }
+
+    
+    alert('Form submitted successfully!');
+});
